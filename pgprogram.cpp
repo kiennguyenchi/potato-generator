8bde7b2b8a7f8a3c05e54358534b8feead914704
gus <61558904+gusmccallum@users.noreply.github.com>
2021-10-07 18:40:25 -0400

    WIP

--- pgprogram.cpp
+++ pgprogram.cpp
@@ -18,21 +18,35 @@ void createOneHTML(string filename, string lang);
 void createManyHTML(string folderName, string lang);
 string checkArguments(int argc, char** argv);
 string checkLanguage(int argc, char** argv);
+bool checkIfConfig(int argc, char** argv);
+void makeDir(string outputDir);
+string readConfig(string fileName);
+
 
 int main(int argc, char** argv)
 {
+    
+
     if(argc >= 2){
         string language = checkLanguage(argc, argv);
         string name = checkArguments(argc, argv);
-        if (name != "terminate"){
+        if (name != "terminate") {
             if (name == "")
                 name = argv[1];
             size_t folderOrFile = name.find(".txt");
             size_t mdFile = name.find(".md");
-            
-            fs::remove_all("./dist");
-            fs::current_path("./");
-            fs::create_directory("dist");
+
+            //if output folder is specified
+            if (readConfig(name).find("output")) {                  
+                string temp = readConfig(name);
+                string outFolder = temp.substr((temp.find("output") + 10), temp.find(("\"\"" - 1), temp.find("./")));
+                std::cout << outFolder;
+            }
+
+            if (readConfig(name).find("input") != string::npos)
+            {
+
+            }
 
             if (folderOrFile > name.size()){
                 if (mdFile > name.size()){
@@ -80,12 +94,17 @@ string checkArguments(int argc, char** argv){
             cout << "*Run the program with command: ./pgprogram inputName\n";
             cout << "The inputName is a text file name or a folder name\n";
             cout << "*To include a input file/folder, include --input or -i before the file/folder name in the arguments.\n";
+            cout << "*To specify an config file, use --config or -c before the config filename.\n";
             cout << "*To see the version of the program, include --version or -v in the arguments.\n";
             cout << "*To need help, include --help or -h in the arguments.\n";
             fName = "terminate";
             break;
         }else if (string(argv[i]) == "--input" || string(argv[i]) == "-i"){
             fName = argv[(i+1)];
+            break;
+        }
+        else if (string(argv[i]) == "--config" || string(argv[i]) == "-c"){
+            fName = argv[(i + 1)];
             break;
         }
     }
@@ -102,4 +121,36 @@ string checkLanguage(int argc, char** argv){
         } 
     }
     return language;
+}
+
+bool checkIfConfig(int argc, char** argv) {
+    bool config = false;
+    for (int i = 1; i < argc; i++) {
+        if (string(argv[i]) == "--config" || string(argv[i]) == "-c") {
+            config = true;
+        }
+    }
+    return config;
+}
+
+void makeDir(string outputDir) {
+    fs::remove_all("./dist");
+    if (outputDir != "") {
+        outputDir = "./" + outputDir;
+        fs::create_directory(outputDir);
+    }
+    else {
+        fs::create_directory("./dist");
+    }
+}
+
+string readConfig(string fileName) {
+    string storeConfig = "";
+    ifstream configFile;
+    configFile.open(fileName, ios::in);
+    if (configFile.is_open()) {
+        getline(configFile, storeConfig, '}');
+        configFile.close();
+    }
+    return storeConfig;
 }
