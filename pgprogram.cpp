8ec2a64d6baf65af627b85a94a110067b3ce90c9
gusmccallum <gusmccallum@hotmail.com>
2021-10-08 16:05:41 -0400

    Fixed issues

--- pgprogram.cpp
+++ pgprogram.cpp
@@ -57,9 +57,6 @@ int main(int argc, char** argv)
                 name = temp.substr(start, (end - start));
             }
 
-            size_t folderOrFile = name.find(".txt");
-            size_t mdFile = name.find(".md");
-
             if (configFlag || name.find("./") != string::npos) {
                 createManyHTML(name, outFolder, language);
             }
@@ -71,7 +68,7 @@ int main(int argc, char** argv)
             }
         }
 
-        else {
+        else if (configFlag == true) {
             cout << "Empty config file. Please try again." << endl;
         }
     }
@@ -81,7 +78,7 @@ int main(int argc, char** argv)
 }
 
 //this function creates a single HTML page
-void createOneHTML(string filename,string outPath, string lang){
+void createOneHTML(string filename,string outPath, string lang){   
     HTMLFile newFile;
     newFile.openFile(filename, lang);
     newFile.setHtmlFile();
@@ -104,10 +101,13 @@ void createManyHTML(string folderName, string outPath, string lang){
 string checkArguments(int argc, char** argv){
     string fName = "";
     
-        if (string(argv[1]) == "--version" || string(argv[1]) == "-v") {
+    for (int i = 1; i < argc; i++) {
+        if (string(argv[i]) == "--version" || string(argv[i]) == "-v") {
             cout << "Potato Generator - Version 0.1";
             fName = "terminate";
-        }else if (string(argv[1]) == "--help" || string(argv[1]) == "-h"){
+            break;
+        }
+        else if (string(argv[i]) == "--help" || string(argv[i]) == "-h") {
             cout << "*Run the program with command: ./pgprogram inputName\n";
             cout << "The inputName is a text file name or a folder name\n";
             cout << "*To include a input file/folder, include --input or -i before the file/folder name in the arguments.\n";
@@ -115,12 +115,18 @@ string checkArguments(int argc, char** argv){
             cout << "*To see the version of the program, include --version or -v in the arguments.\n";
             cout << "*To need help, include --help or -h in the arguments.\n";
             fName = "terminate";
-        }else if (string(argv[1]) == "--input" || string(argv[1]) == "-i"){
-            fName = argv[2];
+            break;
         }
-        else if (string(argv[1]) == "--config" || string(argv[1]) == "-c"){
-            fName = argv[2];
+        else if (string(argv[i]) == "--input" || string(argv[i]) == "-i") {
+            fName = argv[i+1];
+            break;
         }
+        else if (string(argv[i]) == "--config" || string(argv[i]) == "-c") {
+            fName = argv[i+1]; 
+            break;
+        }
+    }
+        
     
     return fName;
 }
@@ -129,11 +135,10 @@ string checkArguments(int argc, char** argv){
 string checkLanguage(int argc, char** argv){
     string language = "";
     for (int i = 1; i < argc; i++) {
-        if (string(argv[i]) == "--lang" || string(argv[i]) == "-l"){
-            language = argv[(i+1)];
-            break;
-        } 
-    }
+        if (string(argv[i]) == "--lang" || string(argv[i]) == "-l") {
+            language = argv[i+1];
+        }
+    }    
     return language;
 }
 
@@ -143,13 +148,13 @@ bool checkIfConfig(int argc, char** argv) {
         if (string(argv[i]) == "--config" || string(argv[i]) == "-c") {
             config = true;
         }
-    }
+    }    
     return config;
 }
 
 void makeDir(string outputDir) {
     fs::remove_all("./dist");
-    if (outputDir != "//dist") {
+    if (outputDir != "./dist") {
         outputDir = "./" + outputDir;
         fs::create_directory(outputDir);
     }
