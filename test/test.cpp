6f196abfa25584754eb6d767d9532bac3ebe5a17
Kien <kiennguyenchi11@gmail.com>
2021-11-17 12:49:40 -0500

    remake test file

---
+++ test/test.cpp
@@ -0,0 +1,24 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <iostream>
+#include "../HTMLFile.h"
+
+TEST_CASE("Check empty files")
+{
+    HTMLFile file;
+    REQUIRE(file.getURL() == "");
+}
+
+TEST_CASE("Check existing files")
+{
+    HTMLFile file;
+    file.openFile("../Sherlock-Holmes-Selected-Stories/Silver Blaze.txt", "fr");
+    REQUIRE(file.getTextFile().size() == 0 );
+}
+
+TEST_CASE("Check non-existing files")
+{
+    HTMLFile file;
+    file.openFile("../Sherlock-Holmes-Selected-Stories/notafile.txt", "fr");
+    REQUIRE(file.getURL() == "");
+}
